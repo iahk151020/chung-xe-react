8391c0b252ecb7c9a5fe2af319063658c48aece1
Quang Khải <khai151020@gmail.com>
2022-03-21 10:23:18 +0700

    add login, logout UI

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Topbar from "./components/topbar/Topbar";
 import Sidebar from "./components/sidebar/Sidebar";
 import Home from './components/pages/Home';
 import CarStatistic from './components/pages/car/CarStatistic';
+import BillByCar from "./components/pages/bill/BillByCar";
+import BillDetail from "./components/pages/bill/BillDetail";
 import "./app.css";
 import {
   BrowserRouter as Router,
@@ -9,22 +11,44 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import BillByCar from "./components/pages/bill/BillByCar";
-import BillDetail from "./components/pages/bill/BillDetail";
+import Login from "./components/login/Login";
 
 function App() {
   return (
     <Router className="App">
-      <Topbar/>
-      <div className="container">
-        <Sidebar/>
+      
+      
         <Switch>
-          <Route exact path="/admin"><Home/></Route>
-          <Route exact path="/admin/cars/statistic"><CarStatistic/></Route>
-          <Route exact path="/admin/cars/statistic/bills/:car_id?/:start_date?/:end_date?"><BillByCar/></Route>
-          <Route exact path="/admin/bills/:bill_id?"><BillDetail/></Route>
+          <Route exact path="/admin/login"><Login/></Route>
+          <Route exact path="/admin">
+            <Topbar/>
+            <div className="container">
+              <Sidebar/>
+              <Home/>
+            </div>
+          </Route>
+          <Route exact path="/admin/cars/statistic">
+            <Topbar/>
+            <div className="container">
+              <Sidebar/>
+              <CarStatistic/>
+            </div>
+          </Route>
+          <Route exact path="/admin/cars/statistic/bills/:car_id?/:start_date?/:end_date?">
+            <Topbar/>
+            <div className="container">
+              <Sidebar/>
+              <BillByCar/>
+            </div>
+          </Route>
+          <Route exact path="/admin/bills/:bill_id?">
+            <Topbar/>
+            <div className="container">
+              <Sidebar/>
+              <BillDetail/>
+            </div>
+          </Route>
         </Switch>
-      </div>
     </Router>
   );
 }
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 import './topbar.css';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 import SettingsIcon from '@mui/icons-material/Settings';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import {Link} from 'react-router-dom';
 
 function Topbar() {
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  
+  
   return (
     <div className='topbar'>
         <div className="topbarWrapper">
@@ -20,7 +36,25 @@ function Topbar() {
                   <img 
                     src="https://genmec.vn/wp-content/uploads/2020/11/bao-cao-su-don-den-gai-deo-ngon-tay.jpg" 
                     alt="avatar" 
-                    className="topAvatar" />              
+                    className="topAvatar" 
+                    onClick={handleClick}  
+                  />      
+                  <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                      'aria-labelledby': 'basic-button',
+                    }}
+                  >
+                    <MenuItem>Profile</MenuItem>
+                    <MenuItem>My account</MenuItem>
+                    <Link style={{color:'inherit', textDecoration:'inherit'}} to="/admin/login">
+                      <MenuItem>Logout</MenuItem>
+                    </Link>
+                    
+                  </Menu>        
                 </div>
         </div>
     </div>
